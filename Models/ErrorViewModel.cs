282829c7988232d2176e78eecec0836d2ad76564
OtavioNardini <otavionardini2@gmail.com>
2025-11-23 15:03:04 -0300

    Primeiro commit

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace PlataformaB2B_A2_TP3.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
